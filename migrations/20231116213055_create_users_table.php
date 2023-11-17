e1d644fd42728127d70d68b2a4443a13b86d1a73
Bianca Sales <salesbyankaa@gmail.com>
2023-11-17 18:01:05 -0300

    config docker and migrations

--- migrations/20231116213055_create_users_table.php
+++ migrations/20231116213055_create_users_table.php
@@ -9,12 +9,18 @@ final class CreateUsersTable extends AbstractMigration
     public function change(): void
     {
         $sql = <<< SQL
-        CREATE TABLE users (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(255) NOT NULL,
-        cpf VARCHAR(14) NOT NULL,
-        email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    type ENUM ('1', '2') NOT NULL,
+    document VARCHAR(14) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NULL,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP,
+    amount VARCHAR(255) NOT NULL,
+    CONSTRAINT document UNIQUE (document),
+    CONSTRAINT email UNIQUE (email)
 );
 SQL;
         $this->execute($sql);
